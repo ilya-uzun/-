03094df1758c6be7420a9c1b5b6212de872d4fa0
Илья <63155683+ilya-uzun@users.noreply.github.com>
2021-06-11 22:25:47 +0000

    Update lab_1.cs

--- lab_1.cs
+++ lab_1.cs
@@ -1,25 +1,12 @@
 using System;
 
-namespace test
+namespace lab_1
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            var result = GetResult(new int[] { 4, 4, 5, 6, 7 });
-            Console.WriteLine(result);
-            Console.Read();
-        }
-
-        static int GetResult(int[] numbers)
-        {
-            int limit = 0;
-            // локальная функция
-            bool IsMoreThan(int number)
-            {
-                return number > limit;
-  
-            return result;
+           
         }
     }
 }
