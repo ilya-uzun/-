cc7331f2faeccb40137d555a637e14b980e1f76e
ilya-uzun <ilya.uzun@bk.ru>
2021-06-14 19:36:57 +0500

    lab 1

--- lab_1.cs
+++ lab_1.cs
@@ -6,7 +6,14 @@ namespace lab_1
     {
         public static void Main(string[] args)
         {
-           
+          string buf_n;
+          string buf_m;
+	  Console.WriteLine("Введите значение переменнвх");
+	  n = Console.ReadLine();
+          m = Console.ReadLine();
+	  int n = int.Parse(buf_n);
+	  int m = int.Parse(buf_m);
+	  Console.WriteLine(
         }
     }
 }
