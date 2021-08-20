57f290a0a95b401a5f60d714fb0f554f70c068be
ilya-uzun <ilya.uzun@bk.ru>
2021-08-20 20:47:42 +0500

    Lab 1 class Task1

--- Lab_2021/lab1/lab1/Program.cs
+++ Lab_2021/lab1/lab1/Program.cs
@@ -2,10 +2,25 @@
 
 namespace lab1
 {
+    class Tacs
+    {
+        private int m = 0;
+        private int n = 0;
+
+        public void print()
+        {
+            n++;
+            int sima = n + m;
+            Console.WriteLine("n+++m  =  " + sima);
+        }
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
         {
+            Tacs tacs = new Tacs();
+            tacs.print();
             Console.WriteLine("Hello World!");
         }
     }
