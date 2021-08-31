5e450f7a768b6be698cb926344efb52138519d22
ilya-uzun <ilya.uzun@bk.ru>
2021-08-31 21:29:48 +0500

    update

--- Lab_2021/lab1/lab1/Program.cs
+++ Lab_2021/lab1/lab1/Program.cs
@@ -27,20 +27,45 @@ namespace lab1
             }
         }
 
-        public void Print()
+        public void PrintAmount()
         {
             amount = m - (++n);
             Console.WriteLine("Вырожение m-++n  =  " + amount);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Вырожение m =  " + m);
+            Console.WriteLine("Вырожение n =  " + n);
         }
 
         public void Comparison()
         {
             m++;
             n--;
-            if (m > n)  Console.WriteLine("m++>--n");
-  
-        }
+            if (m > n)
+            {
+                Console.WriteLine("m++>--n");
+            }
+            else
+            {
+                Console.WriteLine("Условие не выполненно m++>--n");
+            }
+        }//Comparison()
 
+        public void Comparison2()
+        {
+            m--;
+            n++;
+            if (m < n)
+            {
+                Console.WriteLine("m--<++n");
+            }
+            else
+            {
+                Console.WriteLine("Условие не выполненно m--<++n");
+            }
+        }//Comparison2()
     }
 
     class MainClass
@@ -50,7 +75,10 @@ namespace lab1
             Tacs1 tacs = new Tacs1();
             Console.WriteLine("Введите m и n");
             tacs.Read();
+            tacs.PrintAmount();
             tacs.Comparison();
+            tacs.Print();
+            tacs.Comparison2();
             tacs.Print();
 
         }
