4b5572184de02253b3e52f1f212fe0a04d25dc3a
ilya-uzun <ilya.uzun@bk.ru>
2021-10-01 21:15:22 +0500

    Начало лаб2

---
+++ Lab_2021/lab2/lab2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*
+             Задача №6
+             Дана последовательность из n целых чисел. Найти минимальный элемент в этой последовательности.
+            */
+            Console.Write("Введите количесво целых чисел ");
+            try
+            {
+                int buf = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода ");
+            }
+
+            /*
+             Задача № 29
+             Дана последовательность целых чисел, за которой следует 0. Найти количество нечетных элементов этой последовательности.
+            */
+            Console.Write("Введите количесво целых чисел ");
+            try
+            {
+                int buf = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода ");
+            }
+
+            /*
+             Задача № 40
+             S= sin X + sin X^2 + sin X^3 + sin X^4 +...+ sin X^n 
+            */
+
+        }
+    }
+}
